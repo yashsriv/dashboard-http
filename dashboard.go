e818a300ab5aea6e647da4092859c32d706c3690
Yash Srivastav <admin@yashsriv.org>
2017-02-16 12:05:41 +0530

    Add Recovery for Panics

--- dashboard.go
+++ dashboard.go
@@ -5,6 +5,7 @@ import (
 	"github.com/yashsriv/dashboard-http/router"
 
 	"github.com/iris-contrib/middleware/logger"
+	"github.com/iris-contrib/middleware/recovery"
 	"gopkg.in/kataras/iris.v5"
 )
 
@@ -12,6 +13,7 @@ func main() {
 
 	iris.Config.Gzip = true
 	iris.Use(logger.New())
+	iris.Use(recovery.New())
 
 	router.DashboardRoute()
 
